c96d0df595c457fb43c0131518eed14e791a6d08
Chaya Vogel <118293488+chayavogel@users.noreply.github.com>
2024-09-19 13:51:26 -0400

    commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [text, setText] = useState<string>("");
+
   return (
     <View style={styles.container}>
       <Text>Welcome!</Text>
       <StatusBar style="auto" />
+      <TextInput 
+        multiline={true}
+        style={styles.TextInput}
+        value={text}
+        onChangeText={setText}
+      />
+      <Button 
+        title="Save Note"
+        onPress={() => console.log("i was pressed", text)}
+      />
     </View>
   );
 }
@@ -17,4 +32,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInput: {
+    backgroundColor: "#ffb70342",
+    width: "100%",
+    height: 200,
+    fontSize: 16,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    paddingBottom: 20,
+  } 
 });
